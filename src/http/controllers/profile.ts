a38356bd8f43009b21982a00e567447d1949245e
ericvini <ericcobain3@gmail.com>
2025-04-12 10:47:05 -0300

    feat: autenticação com jwt

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,6 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(req: FastifyRequest, reply: FastifyReply) {
+  await req.jwtVerify()
   return reply.status(200).send()
 }
