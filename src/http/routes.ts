d8b8ccd2a8304809a1e2c072143e3072a1582d90
ericvini <ericcobain3@gmail.com>
2025-04-12 13:54:18 -0300

    feat: autenticação da rota de obter perfil e middleware de auth

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,9 +2,10 @@ import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
 import { profile } from './controllers/profile'
+import { verifyJwt } from './verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/session', authenticate)
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
