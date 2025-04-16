e6398d3812252c2e4df01bec04076a46bb7afecb
ericvini <ericcobain3@gmail.com>
2025-04-16 09:17:09 -0300

    feat: controllers create. nearby e search de academias

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
+import { nearby } from './nearby'
+import { search } from './search'
+import { create } from './create'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
